d680f4fdfce2c1397f75ab08f517d25efc209e3a
eduardsdn <eduard.sdn@gmail.com>
2023-03-28 14:56:14 +0200

    added assets

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+import './style.css'
+import DeleteIcon from './assets/images/delete_FILL0_wght400_GRAD0_opsz48.svg'
+
 console.log("Hello world!")
+
+const content = document.querySelector(".content")
+
+const hello = document.createElement("div")
+hello.textContent = "Hello webpack"
+hello.classList.add("hello")
+
+const myIcon = new Image();
+myIcon.src = DeleteIcon;
+
+content.appendChild(myIcon)
+content.appendChild(hello)
